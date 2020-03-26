4fd753bb03306914d45cd653254ad44925adef47
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-03-27 00:10:51 +0400

    add script

---
+++ script.js
@@ -0,0 +1,9 @@
+let money = 100;
+const income = '200';
+const addExpenses = 500; 
+let deposit;
+const mission = true;
+let period = 5;
+
+console.log('количество денег' + money);
+alert('Привет Мир!');
