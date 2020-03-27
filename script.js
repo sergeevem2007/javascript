76cc9f5f15ae959bcfcc2762093f0c5b50dc5be2
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-03-27 18:47:09 +0400

    add some value for variables

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let money = 100;
-const income = '200';
-const addExpenses = 500; 
-let deposit;
-const mission = true;
+let money = 35000;
+let income = 'фриланс';
+let addExpenses = 'Интернет, Такси, Коммуналка'; 
+let deposit = true;
+let mission = 100000;
 let period = 5;
+let budgetDay;
 
-console.log('количество денег' + money);
-alert('Привет Мир!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+budgetDay = money / 30;
+console.log(budgetDay);
