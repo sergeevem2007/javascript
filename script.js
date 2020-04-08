cb75cf1fa59fbfc41ad783891fc256d63c79358d
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-08 12:44:02 +0400

    feat:add some items

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let collection = document.querySelector('.books'),
+    book = document.querySelectorAll('.book'),
+    body = document.querySelector('body'),
+    titleBook = document.querySelectorAll('a'),
+    advertising = document.querySelector('.adv'),
+    chapter = document.querySelectorAll('li'),
+    newChapter = document.createElement('li');
+    
+    collection.prepend(book[1]);
+    book[0].after(book[4]);
+    book[5].after(book[2]);
+
+    body.style.backgroundImage = "url('../image/you-dont-know-js.jpg')";
+
+    titleBook[4].textContent = 'Книга 3. this и Прототипы Объектов';
+
+    advertising.remove();
+
+    chapter[3].after(chapter[6]);
+    chapter[6].after(chapter[8]);
+    chapter[50].after(chapter[48]);
+    chapter[49].before(chapter[55]);
+
+    newChapter.textContent = 'Глава 8: За пределами ES6';
+    chapter[25].append(newChapter);
